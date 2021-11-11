fixed some axis/scaling issues with the graphs, also added colorbar for spectrogram

app.py
@@ -38,7 +38,7 @@ radio = html.Div([
                         "value": 'off',
                     },
                 ],
-                value='on',
+                value='off',
                 id=f"radio-log-scale",
                 labelStyle={"verticalAlign": "middle"},
                 className="plot-display-radio-items",
@@ -50,49 +50,39 @@ radio = html.Div([
 
 
 
-config = {
-    "log_scale": False,
-}
+drf_dir = "C:/Users/yanag/openradar/openradar_antennas_wb_hf/"
+# drf_dir = "C:/Users/yanag/openradar/openradar_antennas_wb_uhf/"
 
 
-# drf_dir = "C:/Users/yanag/openradar/openradar_antennas_wb_hf/"
-drf_dir = "C:/Users/yanag/openradar/openradar_antennas_wb_uhf/"
-# spec_datas = read_digital_rf_data([drf_dir], plot_file=None, plot_type="spectrum", channel="discone",
-#         subchan=0, sfreq=0.0, cfreq=None, atime=0, start_sample=0, stop_sample=1000000, modulus=None, integration=1, 
-#         zscale=(0, 0), bins=1024, log_scale=True, detrend=False,msl_code_length=0,
-#         msl_baud_length=0)
 
 spec_datas = read_digital_rf_data([drf_dir], plot_file=None, plot_type="spectrum", channel="discone",
         subchan=0, sfreq=0.0, cfreq=None, atime=0, start_sample=0, stop_sample=1000000, modulus=10000, integration=1, 
-        zscale=(0, 0), bins=1024, log_scale=True, detrend=False,msl_code_length=0,
+        zscale=(0, 0), bins=1024, log_scale=False, detrend=False,msl_code_length=0,
         msl_baud_length=0)
 
-specgram_datas = read_digital_rf_data([drf_dir], plot_file=None, plot_type="spectrum", channel="discone",
-        subchan=0, sfreq=0.0, cfreq=None, atime=0, start_sample=0, stop_sample=1000000, modulus=10000, integration=1, 
-        zscale=(0, 0), bins=1024, log_scale=True, detrend=False,msl_code_length=0,
-        msl_baud_length=0)
 
 spec_data = spec_datas[0]
 print(f"sfreq: {spec_data['sfreq']}")
 
 sa = SpectrumAnalyzer(number_samples=spec_data['data'].shape[0], sample_frequency=spec_data['sfreq'])
+y_max = max([max(d['data']) for d in spec_datas])
+y_min = min([min(d['data']) for d in spec_datas])
+print(f"range: {y_min}, {y_max}")
+sa.spec.yrange= (y_min, y_max)
+sa.spectrogram.zmin = y_min
+sa.spectrogram.zmax = y_max
+print(f'yrange: {sa.spec.yrange}')
 
 sa.centre_frequency = spec_data['cfreq']
 print(f'center freq: {sa.spec.centre_frequency}')
-print(f"setting data: { spec_data['data'] }")
 
-sa.spec.data = spec_data['data']
-sa.spectrogram.data = spec_data['data']
-print(f"max data: {max(spec_data['data'])}")
-print(f'data: {sa.spec.data}')
 
-print(sa.spec)
+print(f"yrange: {sa.spec.yrange}")
 
 
 app.layout = html.Div(children=[
     html.H1(children='Digital RF Dashboard'),
     html.Button('Reset', id='reset-val', n_clicks=0),
-    html.Button('Play', id='play-val', n_clicks=0),
     radio,
     dcc.Graph(
         id='spectrum-graph',
@@ -115,34 +105,22 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
-    dash.Output('interval-component', 'n_intervals'),
+    # dash.Output('interval-component', 'n_intervals'),
+    dash.Output('interval-component', 'disabled'),
     dash.Input('reset-val', 'n_clicks'),
 )
 def update_interval(reset_clicks):
-    return 0
-
-@app.callback(
-    dash.Output('interval-component', 'disabled'),
-    dash.Input('play-val', 'n_clicks'),
-)
-def update_interval(play_clicks):
-    if play_clicks > 0:
+    if reset_clicks > 0:
         return False
     return True
 
-@app.callback(dash.Output('specgram-graph', 'figure'),
-              dash.Input('interval-component', 'n_intervals'))
-def update_spec_metrics(n):
-    print('data boop')
-    if n < len(spec_datas):
-        d = spec_datas[n]['data']
-        sa.spectrogram.data = d
-
-        zscale_low = np.median(d.min())
-        zscale_high = np.median(d.max())
-        sa.spectrogram.zmin = zscale_low
-        sa.spectrogram.zmax = zscale_high
-    return sa.spectrogram.get_plot()
+# reset data back to beginning every time "reset" button gets pressed
+@app.callback(
+    dash.Output('interval-component', 'n_intervals'),
+    dash.Input('interval-component', 'disabled'),
+)
+def disabled_update(disabled):
+    return 0
 
 
 
@@ -157,15 +135,55 @@ def update_spectrum_graph(n, log_scale):
     prop_id = ctx.triggered[0]['prop_id'].split('.')[0]
     if prop_id == "interval-component":
         if n < len(spec_datas):
-            sa.spec.data        = spec_datas[n]['data']
-            sa.spectrogram.data = spec_datas[n]['data']
+            d = spec_datas[n]['data']
+            if log_scale == 'on':
+                d = 10.0 * np.log10(d + 1e-12)
+            sa.spec.data        = d
 
     else:
         if log_scale == 'on':
-            sa.spec.log_scale = True
+            sa.spec.ylabel          = "Amplitude (dB)" 
+            sa.spec.yrange= (10.0 * np.log10(y_min + 1e-12) - 3, 10.0 * np.log10(y_max + 1e-12) + 10)
+            
         else:
-            sa.spec.log_scale = False
+            sa.spec.ylabel = "Amplitude" 
+            sa.spec.yrange         = (y_min, y_max)
+
     return sa.plot
+
+
+
+
+@app.callback(dash.Output('specgram-graph', 'figure'),
+              dash.Input('interval-component', 'n_intervals'),
+              dash.Input("radio-log-scale", "value"))
+def update_specgram_graph(n, log_scale):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return sa.spectrogram.get_plot()
+
+    prop_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    if prop_id == "interval-component":
+        if n < len(spec_datas):
+            d = spec_datas[n]['data']
+            if log_scale == 'on':
+                d = 10.0 * np.log10(d + 1e-12)
+            sa.spectrogram.data = d
+
+    else:
+        if log_scale == 'on':
+            sa.spectrogram.zlabel =  "Power (dB)"
+            sa.spectrogram.zmin     = 10.0 * np.log10(y_min + 1e-12) - 3
+            sa.spectrogram.zmax     = 10.0 * np.log10(y_max + 1e-12) + 10
+            
+        else:
+            sa.spectrogram.zlabel =  "Power"
+            sa.spectrogram.zmin     = y_min
+            sa.spectrogram.zmax     = y_max
+
+    return sa.spectrogram.get_plot()
+
+
 
 
 

specgram_graph.py
@@ -17,22 +17,25 @@ class Spectrogram():
                  nyquist_stopband=1,
                  ypixel=2,
                  plot_time=20,
+                 zlabel='Power (dB)',
                  zmin=-80,
                  zmax=0,
                  cmap='jet'):
         
-        self._width = width
-        self._height = height
-        self._image_width = image_width
-        self._image_height = image_height
-        self._sample_frequency = sample_frequency
+        self._width             = width
+        self._height            = height
+        self._image_width       = image_width
+        self._image_height      = image_height
+        self._sample_frequency  = sample_frequency
         self._decimation_factor = decimation_factor
-        self._centre_frequency = centre_frequency
-        self._nyquist_stopband = nyquist_stopband
-        self._ypixel = ypixel
-        self._data = np.ones((self._image_height, self._image_width, 3), dtype=np.uint8)*128
+        self._centre_frequency  = centre_frequency
+        self._nyquist_stopband  = nyquist_stopband
+        self._ypixel      = ypixel
+        self._data        = np.ones((self._image_height, self._image_width, 3), dtype=np.uint8)*128
         self._data_status = False
-        self.cmap = cmap
+        self.cmap         = cmap
+        self._zlabel = zlabel
+
         
         self._image_x = -(self._sample_frequency/self._decimation_factor)/2 + self._centre_frequency
         self._image_y = 0
@@ -42,12 +45,33 @@ class Spectrogram():
             self._nyquist_stopband + self._centre_frequency
         
         self._plot_time = self._image_height
-        self.zmin = zmin
-        self.zmax = zmax
+        self._zmin = zmin
+        self._zmax = zmax
         # self.enable_updates = False
         self.enable_updates = True
+
+        cm = plt.get_cmap(self.cmap)
+        cm = self.matplotlib_to_plotly(cm, 255)
+
+        # to add color bar to plot
+        dummy_trace = {
+            'x': [None],
+            'y': [None],
+            'mode': 'markers',
+            'marker': {
+                'colorscale': cm,
+                'cmin': self._zmin,
+                'cmax': self._zmax,
+                'showscale':True,
+                'colorbar': {
+                    'thickness':20,
+                    'title': {'text': self._zlabel},
+                    
+                },
+            }
+        }
         
-        self._plot = go.FigureWidget(layout={
+        self._plot = go.FigureWidget(data=[dummy_trace], layout={
             'height' : self._height,
             'width' : self._width,
             'yaxis' : {
@@ -69,8 +93,9 @@ class Spectrogram():
                 't':25,
                 'b':25,
                 'l':25,
-                'r':25,
-        }})
+                'r':25,},
+
+        })
         
         img = Image.fromarray(self._data, 'RGB')
         self._plot.add_layout_image(
@@ -84,11 +109,24 @@ class Spectrogram():
                 sizey=self._plot_time,
                 sizing='stretch',
                 opacity=1,
-                layer="below")
+                layer="below",
+                )
         )
         
         self._update_image()
-        
+
+
+    # Taken from https://stackoverflow.com/questions/55447131/how-to-add-a-colorbar-to-an-already-existing-plotly-figure
+    def matplotlib_to_plotly(self,cmap, pl_entries):
+        h = 1.0/(pl_entries-1)
+        pl_colorscale = []
+
+        for k in range(pl_entries):
+            C = list(map(np.uint8, np.array(cmap(k*h)[:3])*255))
+            pl_colorscale.append([k*h, 'rgb'+str((C[0], C[1], C[2]))])
+
+        return pl_colorscale
+            
     @property
     def template(self):
         return self._plot.layout.template
@@ -106,6 +144,7 @@ class Spectrogram():
         if self.enable_updates:
             self._data_status = True
             # value = np.fft.fftshift(data) # FFT Shift
+
             value = data
             value = np.array(np.interp(value, (self.zmin, self.zmax), (0, 1)), dtype=np.single) # Scale Z-Axis
             value = np.resize(signal.resample(value, self._image_width), (1, self._image_width)) # Resample X-Axis
@@ -117,7 +156,7 @@ class Spectrogram():
             img = Image.fromarray(self._data, 'RGB') # Create image
             self._plot.update_layout_images({'source' : img}) # Set as background
             self._data_status = False
-        
+
     @property
     def ypixel(self):
         return self._ypixel
@@ -199,7 +238,51 @@ class Spectrogram():
             }})
             self._plot.update_layout_images({'sizey' : self._plot_time})
             self._update_image()
+
+    @property
+    def zlabel(self):
+        return self._zlabel
         
+    @zlabel.setter
+    def zlabel(self, zlabel):
+        self._zlabel = zlabel
+        self._plot.update_traces(
+            {'marker': {
+                'colorbar': {
+                    # 'title': {'text': self._zlabel},
+                    'title': {'text': zlabel},
+                }}}
+        )
+        
+    @property
+    def zmin(self):
+        return self._zmin
+        
+    @zmin.setter
+    def zmin(self, zmin):
+        self._zmin = zmin
+        self._plot.update_traces(
+            {'marker': {
+               'cmin': self._zmin,
+            }}
+        )   
+        
+
+    @property
+    def zmax(self):
+        return self._zmax
+        
+    @zmax.setter
+    def zmax(self, zmax):
+        self._zmax = zmax
+        self._plot.update_traces(
+            {'marker': {
+               'cmax': self._zmax,
+            }}
+        )   
+
+
+
     def _update_image(self):
         self._lower_limit = (-(self._sample_frequency/self._decimation_factor)/2) * self._nyquist_stopband + self._centre_frequency 
         self._upper_limit = ((self._sample_frequency/self._decimation_factor)/2) * self._nyquist_stopband + self._centre_frequency
@@ -212,6 +295,6 @@ class Spectrogram():
         self._plot.update_layout_images({'source' : img,
                                          'x' : self._image_x,
                                          'sizex' : (self._sample_frequency/self._decimation_factor)})
-    
+
     def get_plot(self):
         return self._plot

spectrum_graph.py
@@ -17,8 +17,7 @@ class Spectrum():
                  data_windowsize=16,
                  spectrum_mode=True,
                  decimation_factor=2,
-                 x_data=None,
-                 log_scale=False):
+                 x_data=None):
     
         self._y_data = plot_data
         self._y_data_current = plot_data
@@ -50,13 +49,12 @@ class Spectrum():
         self._update_ddc_counter  = 0
         self.ddc_centre_frequency = 0
         self.data_windowsize      = data_windowsize
-        self.post_process         = 'max' #'none'
+        self.post_process         = 'none'
         # self.enable_updates       = False
         self.enable_updates       = True
         self.display_min          = False
         self.display_max          = False
         self.display_ddc_plan     = []
-        self._log_scale           = log_scale
         
         
         if (np.ceil(self._centre_frequency/(self._sample_frequency/2)) % 2) == 0:
@@ -76,7 +74,7 @@ class Spectrum():
             'yaxis' : {
                 'title' : self._ylabel,
                 'range' : self._yrange,
-                'autorange' : False
+                'autorange' : False,
             },
             'margin' : {
                 't':25,
@@ -305,7 +303,6 @@ class Spectrum():
                 self._update_ddc_counter = 0
             else:
                 self._update_ddc_counter = self._update_ddc_counter + 1
-
         self._update_x_axis()
     
     @property
@@ -351,19 +348,7 @@ class Spectrum():
     @height.setter
     def height(self, height):
         self._plot.layout.height = height
-        
-    @property
-    def log_scale(self):
-        return self._log_scale
     
-    @log_scale.setter
-    def log_scale(self, log_scale):
-        if log_scale and not self._log_scale:
-            self.ylabel += "(dB)"
-        elif not log_scale and self.log_scale:
-            self.ylabel = self.ylabel[:-4]
-
-        self._log_scale = log_scale
 
     @property
     def number_samples(self):
@@ -420,10 +405,7 @@ class Spectrum():
             fdata = np.minimum(self._y_data_current, fdata)
         else:
             pass
-
-        if self._log_scale:
-            fdata= 10.0 * np.log10(fdata + 1e-12)
-
+            
         return fdata
         
     def _clear_plot(self):
@@ -452,10 +434,9 @@ class Spectrum():
 
         self._plot.data[1].update({
             'x':self._x_data,
-            'y':np.zeros(len(self._x_data)) + min(self._y_data) - 3
-            # 'y':np.zeros(len(self._x_data)) - 300
-            # 'y':self._y_data - max(self.yrange)
+            'y':np.zeros(len(self._x_data)) + self._yrange[0]
         })
+
         if self.post_process == 'max':
             self._y_data = np.zeros(len(self._x_data)) - 300
             self._y_data_current = np.zeros(self._number_samples) - 300