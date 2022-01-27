07929e33e59b33632a33994d75e0043dd803774d
Yana Galina <yanag@bu.edu>
2022-01-27 11:54:33 -0500

    Reorganized file structure, fixing bugs with streaming data from redis

--- front_end/app.py
+++ front_end/app.py
@@ -10,23 +10,16 @@ It also defines the layout and functionality of the app.
 # visit http://127.0.0.1:8050/ in your web browser.
 import re
 import time
+import json
 
 import dash
 from dash import dcc
 from dash import html
-import plotly.express as px
-import pandas as pd
+import dash_bootstrap_components as dbc
 import numpy as np
 import redis
-import json
-
-
-import dash_bootstrap_components as dbc
 
 from spectrum_analyzer import SpectrumAnalyzer
-from digital_rf_utils import *
-
-
 
 
 
@@ -35,8 +28,6 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY], suppress_call
 
 redis_instance = redis.Redis(host='localhost', port=6379, db=0)
 pubsub = redis_instance.pubsub(ignore_subscribe_messages=True)
-
-
 
 
 # create radio option components
@@ -103,19 +94,21 @@ app.layout = dbc.Container([
                 ),
                 dbc.Button(
                     'Choose input directory', 
-                    id='input-dir-val', 
-                    n_clicks=0,
-                    color="primary",
-                ),
-                dbc.Button(
-                    'redis', 
-                    id='redis-button', 
+                    id='input-dir-button', 
                     n_clicks=0,
                     color="primary",
                 ),
                 html.Br(),
                 html.Div(id='drf-err'),
-                html.Div(id='channel-div', style={'width': '100%'}),
+                dcc.Loading(
+                    id="channel-loading",
+                    children=[
+                        html.Div(id='channel-div', style={'width': '100%'}),
+                        
+                    ],
+                    type="circle",
+                ),
+                
                 html.Div(id='sample-div', style={'width': '100%'},),
                         
                 html.Div(id='bins-div', style={'width': '100%'},),
@@ -148,7 +141,7 @@ app.layout = dbc.Container([
                             id='interval-component',
                             interval=1*100, # in milliseconds
                             n_intervals=0,
-                            max_intervals=100,
+                            # max_intervals=100,
                             disabled=True,
                         ),
                     dcc.Graph(
@@ -163,11 +156,19 @@ app.layout = dbc.Container([
                     id='redis-interval',
                     interval=100, # in milliseconds
                     n_intervals=0,
-                    max_intervals=1000,
+                    # max_intervals=1000,
                     disabled=True,
                 ),
         ], width=True,),
     ]),
+    html.Div(id='reading-stream-graph-interval-placeholder', n_clicks=0,),
+    html.Div(id='spectrum-graph-interval-placeholder', n_clicks=0,),
+    html.Div(id='request-redis-interval-placeholder', n_clicks=0,),
+    html.Div(id='reading-stream-redis-interval-placeholder', n_clicks=0,),
+
+
+    
+
 
 ], fluid=True)
 
@@ -183,8 +184,9 @@ def drf_responses_handler(msg):
 
 @app.callback(
     dash.Output(component_id='channel-div', component_property='children'),
-    dash.Input('redis-button', 'n_clicks'),
+    dash.Input('input-dir-button', 'n_clicks'),
     dash.State('drf-path', 'value'),
+
 )
 def start_redis_stream(n, drf_path):
     # if (n % 2) == 1:
@@ -195,14 +197,12 @@ def start_redis_stream(n, drf_path):
         next_req_id = int(req_id) + 1
         redis_instance.set('request-id', next_req_id)
 
-        # pubsub.psubscribe(**{f'responses:{req_id}:*': drf_responses_handler})
         pubsub.psubscribe(f'responses:{req_id}:*')
 
-        print(f"publishing request for {drf_path}")
+        print(f"publishing request {req_id} for {drf_path}")
         # make a request for the channels from drf_path
         redis_instance.publish(f'requests:{req_id}:channels', drf_path)
 
-        # thread = pubsub.run_in_thread(sleep_time=0.01)
         for msg in pubsub.listen():
             print(f"got response message: {msg}")
             channel = msg['channel'].decode()
@@ -227,14 +227,9 @@ def start_redis_stream(n, drf_path):
                  ]
 
                 return children
-                break
 
 
-        # time.sleep(5)
-
-        # thread.stop()
-        # redis_instance.rpush('channel-requests', json.dumps({'req_id': req_id, 'drf_path': drf_path}))
-
+    
         global redis_data
         # for drf files, we want to push all data we haven't into a queue which gets outputted
         redis_data = []
@@ -247,7 +242,7 @@ def start_redis_stream(n, drf_path):
 
 @app.callback(
     dash.Output(component_id='load-val', component_property='disabled'),
-    dash.Input('redis-button', 'n_clicks'),
+    dash.Input('input-dir-button', 'n_clicks'),
 )
 def redis_update_load_data_button(n):
     if n < 1: return True
@@ -257,10 +252,8 @@ def redis_update_load_data_button(n):
 
 
 @app.callback(
-    dash.Output('interval-component', 'disabled'),
-    dash.Output('interval-component', 'max_intervals'),
     dash.Output('drf-err', 'children'),
-    dash.Output('redis-interval', 'disabled'),
+    dash.Output('request-redis-interval-placeholder', 'n_clicks'),
     dash.Input('load-val', 'n_clicks'),
     dash.State('drf-path', 'value'),
     dash.State({'type': 'channel-picker', 'index': dash.ALL,}, 'value'),
@@ -269,7 +262,7 @@ def redis_update_load_data_button(n):
 )
 def send_redis_request_data(n_clicks, drf_path, channel, sample_range, bins):
     if n_clicks < 1:
-         return True, 0, None, True
+         return None, dash.no_update
     req = {
         'drf_path'     : drf_path,
         'channel'      : channel[0],
@@ -320,34 +313,50 @@ def send_redis_request_data(n_clicks, drf_path, channel, sample_range, bins):
             data_queue = []
             global data_q_idx
             data_q_idx = 0
-            return False, 100, None, False
+            return None, 1
 
 
-    return True, 0, None, True
+    return None, 0
 
 
 
-@app.callback(dash.Output('placeholder', 'n_clicks'),
-              dash.Input('redis-interval', 'n_intervals'))
+@app.callback(
+    dash.Output('reading-stream-graph-interval-placeholder', 'n_clicks'),
+    dash.Output('reading-stream-redis-interval-placeholder', 'n_clicks'),
+    dash.Input('redis-interval', 'n_intervals'))
 def read_redis_stream(n_intervals):
-    if n_intervals == 0: return None
+    if n_intervals == 0: return dash.no_update
 
-    streamname = 'example'
     print("reading redis stream!")
-    # rstrm = redis_instance.xread({streamname: '$'}, None, 0)
     global last_r_id
     global data_queue
     print(f'last_r_id: {last_r_id}')
+    finished = False
     if last_r_id is None:
-        # rstrm = redis_instance.xrevrange(f'responses:{req_id}:stream', count=1)
         rstrm = redis_instance.xrange(f'responses:{req_id}:stream')
         if (len(rstrm) == 0):
             print("no update")
-            return dash.no_update
-        print(f'rstrm: {rstrm}')
-        print(f'foo {rstrm[0][0]}')
+            return dash.no_update, dash.no_update
+        
         last_r_id = rstrm[-1][0].decode()
         print(f'last_r_id: {last_r_id}')
+
+        print(f"data is: {len(rstrm)}")
+
+        for d in rstrm:
+            datum = json.loads(d[1][b'data'])
+            if 'status' in datum:
+                # stream has finished
+                if datum['status'] == 'DONE':
+                    # unsubscribe from updates
+                    finished = True
+                    pubsub.punsubscribe(f'responses:{req_id}:*')
+                    return 1, 0 # enable graph interval, disable further redis streaming
+            else:
+                data_queue.append(datum)
+
+        return 1, dash.no_update # enable graph interval
+
 
     else:
         rstrm = redis_instance.xrevrange(f'responses:{req_id}:stream', min=f'({last_r_id}')
@@ -356,110 +365,31 @@ def read_redis_stream(n_intervals):
             return dash.no_update
         last_r_id = rstrm[-1][0].decode()
 
+        print(f"data is: {len(rstrm)}")
 
-    print(f"data is: {len(rstrm)}")
-    # xlist = json.loads(rstrm[0][1][0][1][b'data'])
-    xlist = json.loads(rstrm[0][1][b'data'])
-    print(f"data is: {len(xlist)}")
+        for d in rstrm:
+            datum = json.loads(d[1][b'data'])
+            if 'status' in datum:
+                # stream has finished
+                if datum['status'] == 'DONE':
+                    # unsubscribe from updates
+                    finished = True
 
-    global r_data
-    r_data = xlist
-    data_queue.extend([json.loads(d[1][b'data']) for d in rstrm])
+                    pubsub.punsubscribe(f'responses:{req_id}:*')
+                    return dash.no_update, 0 # enable graph interval, disable furthe redis streaming
+
+            else:
+                data_queue.append(datum)
+
+        # data_queue.extend([json.loads(d[1][b'data']) for d in rstrm])
     
-    # sa.spec.data = xlist
-    # sa.spectrogram.data = xlist
-
-
-    return None
-
-
-# @app.callback(
-#     dash.Output('interval-component', 'max_intervals'),
-#     dash.Output('drf-err', 'children'),
-#     dash.Input('load-val', 'n_clicks'),
-#     dash.State('drf-path', 'value'),
-#     dash.State({'type': 'channel-picker', 'index': dash.ALL,}, 'value'),
-#     dash.State({'type': 'range-slider', 'index': dash.ALL,}, 'value'),
-#     dash.State({'type': 'bins-slider', 'index': dash.ALL,}, 'value'),
-# )
-# def update_drf_data(n_clicks, drf_path, channel, sample_range, bins):
-#     """
-#     Load metadata for Digital RF file when "load data" button is clicked,
-#     update the "max intervals" component with the length of the data, 
-#     or alternatively output an an error message if the specified path
-#     does not contain Digital RF data
-#     """
-#     global spec_datas
-
-#     # clear spectrogram plot when reset is clicked
-#     print(f"called update drf data, n: {n_clicks}, chan: {channel}, samp range: {sample_range}")
-#     if n_clicks < 1:
-#         return 100, None
-
-#     try:
-#         spec_datas = read_digital_rf_data([drf_path], plot_file=None, plot_type="spectrum", channel=channel[0],
-#             subchan=0, sfreq=0.0, cfreq=None, atime=0, start_sample=sample_range[0][0], stop_sample=sample_range[0][1], modulus=10000, integration=1, 
-#             zscale=(0, 0), bins=2**bins[0], log_scale=False, detrend=False,msl_code_length=0,
-#             msl_baud_length=0)
-
-
-#     except Exception as e:
-#         # output error message
-#         print(e)
-#         return dash.no_update, str(e)
-
-
-#     # get metadata from redis
-#     # print("reading redis stream?")
-#     # rstrm = redis_instance.xread({streamname: '$'}, None, 0)
-#     # print("foo")
-#     # xlist = json.loads(rstrm[0][1][0][1][b'data'])
-#     # print(f"data is: {xlist}")
-
-
-
-#     # set metadata for plots
-#     sa.spectrogram.clear_data()
-
-#     spec_data  = spec_datas['data'][0]
-#     sfreq      = spec_datas['metadata']['sfreq']
-#     n_samples  = spec_data['data'].shape[0]
-
-#     global y_min
-#     global y_max
-#     y_max = max([max(d['data']) for d in spec_datas['data']])
-#     y_min = min([min(d['data']) for d in spec_datas['data']])
-
-#     # set axes and other basic info for plots
-#     sa.spec.yrange      = (y_min, y_max)
-#     sa.spectrogram.zmin = y_min
-#     sa.spectrogram.zmax = y_max
-
-#     sa.centre_frequency = spec_datas['metadata']['cfreq']
-
-
-#     sa.spec.sample_frequency        = sfreq
-#     sa.spectrogram.sample_frequency = sfreq
-#     sa.spec.number_samples          = n_samples
-#     sa.spectrogram.number_samples   = n_samples
-
-
-#     return len(spec_datas['data']), None
-
-# @app.callback(
-#     dash.Output(component_id='load-val', component_property='disabled'),
-#     dash.Input('input-dir-val', 'n_clicks'),
-# )
-# def update_load_data_button(n):
-#     if n < 1: return True
-
-#     return False
+        return dash.no_update, dash.no_update 
 
 
 
 @app.callback(
     dash.Output(component_id='sample-div', component_property='children'),
-    dash.Input('redis-button', 'n_clicks'),
+    dash.Input('input-dir-button', 'n_clicks'),
 )
 def update_sample_slider(n):
     if n < 1: return None
@@ -492,7 +422,7 @@ def update_sample_slider(n):
 
 @app.callback(
     dash.Output(component_id='bins-div', component_property='children'),
-    dash.Input('redis-button', 'n_clicks'),
+    dash.Input('input-dir-button', 'n_clicks'),
 )
 def update_bins_slider(n):
     if n < 1: return None
@@ -550,69 +480,83 @@ def update_metadeta_output(n):
 
 
 
-
-
 # @app.callback(
-#     dash.Output('interval-component', 'disabled'),
-#     dash.Input('reset-val', 'n_clicks'),
-#     dash.State("radio-log-scale", "value")
+#     dash.Output('interval-component', 'n_intervals'),
+#     dash.Input('interval-component', 'disabled'),
 # )
-# def update_interval(reset_clicks, log_scale):
-#     """
-#     Enable playback of digital RF file when reset is clicked
+# def disabled_update(disabled):
+#     """ reset data back to beginning every time "reset" button gets pressed """
 
-#     """
-#     # clear spectrogram plot when reset is clicked
-#     sa.spectrogram.clear_data()
+#     if not disabled:
+#         # make sure data isn't hidden if playback is going on
+#         sa.spec.show_data()
 
-#     # handle the log scales
-#     if log_scale == 'on':
-#         sa.spec.ylabel          = "Power Spectrum (dB)" 
-#         sa.spec.yrange= (10.0 * np.log10(y_min + 1e-12) - 3, 10.0 * np.log10(y_max + 1e-12) + 10)
-#         sa.spectrogram.zlabel   =  "Power (dB)"
-#         sa.spectrogram.zmin     = 10.0 * np.log10(y_min + 1e-12) - 3
-#         sa.spectrogram.zmax     = 10.0 * np.log10(y_max + 1e-12) + 10
-        
-#     else:
-#         sa.spec.ylabel        = "Power Spectrum" 
-#         sa.spec.yrange        = (y_min, y_max)
-#         sa.spectrogram.zlabel = "Power"
-#         sa.spectrogram.zmin   = y_min
-#         sa.spectrogram.zmax   = y_max
+#     return 0
 
-
-#     if reset_clicks > 0:
-#         return False
-
-#     # playback should be disabled before reset button is clicked
-#     return True
 
 @app.callback(
-    dash.Output('interval-component', 'n_intervals'),
-    dash.Input('interval-component', 'disabled'),
+    dash.Output('interval-component', 'disabled'),
+    dash.Input('reading-stream-graph-interval-placeholder', 'n_clicks'),
+    dash.Input('spectrum-graph-interval-placeholder', 'n_clicks'),
 )
-def disabled_update(disabled):
-    """ reset data back to beginning every time "reset" button gets pressed """
+def update_graph_interval(reading_interval, spectrum_interval):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return dash.no_update
+    prop_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    if not disabled:
-        # make sure data isn't hidden if playback is going on
-        sa.spec.show_data()
+    if prop_id == "reading-stream-graph-interval-placeholder":
+        disabled = not bool(reading_interval)
+        if not disabled:
+            sa.spec.show_data()
 
-    return 0
+    if prop_id == "spectrum-graph-interval-placeholder":
+        disabled = not bool(spectrum_interval)
+
+    print(f'Interval (graph updating) component is disabled: {disabled}')
+
+    return disabled
+
+
+@app.callback(
+    dash.Output('redis-interval', 'disabled'),
+    dash.Input('request-redis-interval-placeholder', 'n_clicks'),
+    dash.Input('reading-stream-redis-interval-placeholder', 'n_clicks'),
+)
+def update_redis_interval(req_interval, reading_interval):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return dash.no_update
+    prop_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if prop_id == "request-redis-interval-placeholder":
+        print(f"Update redis interval, from the request function: {req_interval}")
+        disabled = not bool(req_interval)
+
+    if prop_id == "reading-stream-redis-interval-placeholder":
+        print(f"Update redis interval, from the reading function: {reading_interval}")
+        disabled = not bool(reading_interval)
+
+    print(f'Redis Interval component is disabled: {disabled}')
+
+    return disabled
+
+
+
 
 
 
 @app.callback(dash.Output('spectrum-graph', 'figure'),
+              dash.Output('spectrum-graph-interval-placeholder', 'n_clicks'),
               dash.Input('interval-component', 'n_intervals'),
-              dash.State('placeholder', 'n_clicks'),
               dash.Input("radio-log-scale", "value"))
-def update_spectrum_graph(n, placeholder, log_scale):
+def update_spectrum_graph(n, log_scale):
     """ update the spectrum graph with new data, every time
     the Interval component fires"""
 
     ctx = dash.callback_context
     if not ctx.triggered or not spec_datas:
-        return sa.plot
+        return sa.plot, 0
 
     prop_id = ctx.triggered[0]['prop_id'].split('.')[0]
     if prop_id == "interval-component": # intervale component has fired
@@ -626,47 +570,33 @@ def update_spectrum_graph(n, placeholder, log_scale):
             print(f"redis data is : {d}")
             sa.spec.data        = d
             data_q_idx += 1
-            return sa.plot
+            return sa.plot, dash.no_update
+        elif len(data_queue) > 0:
+            return dash.no_update, 0 # disable the interval from firing 
         else:
-            return dash.no_update
+            return dash.no_update, dash.no_update # disable the interval from firing 
 
-
-
-        # if n < len(spec_datas['data']):
-        #     print(f"data {n}")
-        #     d = spec_datas['data'][n]['data']
-        #     if log_scale == 'on':
-        #         d = 10.0 * np.log10(d + 1e-12)
-            # sa.spec.data        = d
-
-    elif prop_id == "placeholder":
-        print("updating spectrum graph using redis data")
-        d = np.asarray(r_data)
-        if log_scale == 'on':
-            d = 10.0 * np.log10(d + 1e-12)
-        print(f"redis data is : {d}")
-        sa.spec.data        = d
 
     else:
         # log scale option has been modified
+        print("modifying log scale")
         if log_scale == 'on':
             sa.spec.ylabel = "Amplitude (dB)" 
             sa.spec.yrange = (10.0 * np.log10(y_min + 1e-12) - 3, 10.0 * np.log10(y_max + 1e-12) + 10)
+            sa.spec.data   =  10.0 * np.log10(sa.spec.data + 1e-12)  
             
         else:
             sa.spec.ylabel = "Amplitude" 
             sa.spec.yrange = (y_min, y_max)
 
-    return sa.plot
+    return sa.plot, dash.no_update
 
 
 
 @app.callback(dash.Output('specgram-graph', 'figure'),
               dash.Input('interval-component', 'n_intervals'),
-              dash.Input('placeholder', 'n_clicks'),
-
               dash.Input("radio-log-scale", "value"))
-def update_specgram_graph(n, placeholder, log_scale):
+def update_specgram_graph(n, log_scale):
     """ update the spectogram plot with new data, every time
     the Interval component fires"""
 
@@ -683,24 +613,11 @@ def update_specgram_graph(n, placeholder, log_scale):
             d = np.asarray(data_queue[data_q_idx])
             if log_scale == 'on':
                 d = 10.0 * np.log10(d + 1e-12)
-                
+
             sa.spectrogram.data = d
             data_q_idx += 1
         else:
             return dash.no_update
-
-        # if n < len(spec_datas['data']):
-        #     d = spec_datas['data'][n]['data']
-        #     if log_scale == 'on':
-        #         d = 10.0 * np.log10(d + 1e-12)
-
-        #     sa.spectrogram.data = d
-    elif prop_id == "placeholder":
-        print("updating specgram graph using redis data")
-        d = np.asarray(r_data)
-        if log_scale == 'on':
-            d = 10.0 * np.log10(d + 1e-12)
-        sa.spectrogram.data = d
 
     else:
         if log_scale == 'on': # log scale option has changed
